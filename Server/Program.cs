bcb704f770f53bcb6259d0166062e650dc6c8b63
Nevron-Software <email@nevron.com>
2023-06-20 15:24:32 +0300

    Removing app settings value

--- Server/Program.cs
+++ Server/Program.cs
@@ -14,7 +14,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 // add db context
-string connString = builder.Configuration.GetConnectionString("DBConnectionString");
+
+string? connString = Environment.GetEnvironmentVariable("CONN_STR");
+
+if (string.IsNullOrEmpty(connString))
+    connString = "";
+
 builder.Services.AddDbContext<NDbContext>(options => { options.UseSqlServer(connString); });
 builder.Services.AddTransient<INOrderBlService, NOrderBl>();
 
